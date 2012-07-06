aa8153889faad9e6b8267429882caabe5f8cb0d5
yiwinking <yiwinking@Gmail.com>
2012-07-06 15:21:34 +0800

    update

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Html5::Application.config.secret_token = 'f5672ba89647d9045d2ea865c06e90407cb7b4474deb7df0c235248c1aa9f65725ae935ad845d856cf84b8743697d642010adf0a0f4961514c7a20a7cd39a6ea'
